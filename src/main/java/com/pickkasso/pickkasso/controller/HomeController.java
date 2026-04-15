4c61c4f941126c363fcf397e8fe0bd01472862f2
근찬 <geunchanlee.dev@gmail.com>
2026-04-15 16:10:58 +0900

    feat: 회원가입 페이지 구현 + docs에 페이지 설명 추가

--- src/main/java/com/pickkasso/pickkasso/controller/HomeController.java
+++ src/main/java/com/pickkasso/pickkasso/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.pickkasso.pickkasso.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,33 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "common/login";
+    }
+
+    @GetMapping("/signup")
+    public String signupIndex() {
+        return "redirect:/signup/user";
+    }
+
+    @GetMapping("/signup/user")
+    public String signupUser() {
+        return "common/signup-user";
+    }
+
+    @GetMapping("/signup/photographer")
+    public String signupPhotographer() {
+        return "common/signup-photographer";
+    }
+
+    // TODO(backend): 실제 회원 생성 로직으로 교체할 것
+    // SignupController 분리 + SignupRequest DTO + SignupService + Repository 구현 필요
+    // 성공 시 redirect:/login?signup=success 로 변경
+    @PostMapping({"/signup/user", "/signup/photographer"})
+    public String signupStub() {
+        return "common/signup-pending";
     }
 }
