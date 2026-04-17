668f6bea66e8c3a55080780f18a52e97ec9460fa
근찬 <geunchanlee.dev@gmail.com>
2026-04-17 10:15:11 +0900

    feat: 검색 화면 구현

--- src/main/java/com/pickkasso/pickkasso/item/controller/ItemController.java
+++ src/main/java/com/pickkasso/pickkasso/item/controller/ItemController.java
@@ -1,18 +1,26 @@
 package com.pickkasso.pickkasso.item.controller;
 
+import com.pickkasso.pickkasso.global.tag.TagService;
 import com.pickkasso.pickkasso.item.dto.ItemSearchFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Collections;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
+    private final TagService tagService;
+
     @GetMapping("/search")
-    public String searchItem(@ModelAttribute ItemSearchFormDto itemSearchFormDto) {
-        System.out.println(itemSearchFormDto);
+    public String searchItem(@ModelAttribute ItemSearchFormDto itemSearchFormDto, Model model) {
+        model.addAttribute("tags", tagService.findAllTagReference());
+        model.addAttribute("items", Collections.emptyList());
+        model.addAttribute("totalCount", 0);
         return "search/itemSearchForm";
     }
 }
