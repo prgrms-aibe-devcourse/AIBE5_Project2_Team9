1f167292561da01050af29c60a4723d22b8b6630
Mi-no-Kim <71382553+Mi-no-Kim@users.noreply.github.com>
2026-04-17 17:46:36 +0900

    Reapply "feat: item entity 생성"
    This reverts commit f580769c7ec10192c9b551829457b3fa98b22d5d.

--- src/main/java/com/pickkasso/pickkasso/global/region/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pickkasso.pickkasso.global.region;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
